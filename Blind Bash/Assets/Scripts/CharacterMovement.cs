d91808dd8a5ecf6d171714c3dec0409b5a3329a9
TuuriTuu <gtutu007@edu.xamk.fi>
2025-10-03 13:55:52 +0300

    Added own test scene. Refined character movement script: acceleration and sprite rotation

--- Blind Bash/Assets/Scripts/CharacterMovement.cs
+++ Blind Bash/Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 // added a random comment for classroom activity ~Tuomas
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed = 5f; //movement speed.
+    public float maxSpeed = 30; //movement speed.
+    public float speed = 0; // starting speed
+    public float acceleration = 5.5f; //acc. multiplier
 
     private Rigidbody2D rb; //rigidbody reference
+    private SpriteRenderer sprite; //sprite renderer ref.
     private Vector2 moveDirection = Vector2.zero; //current movement direction
     private bool canMove = true; //true = waiting for input, false = moving.
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -35,26 +39,41 @@ public class CharacterMovement : MonoBehaviour
         // Apply velocity if currently moving
         if (!canMove)
         {
+            if (speed < maxSpeed)
+            {
+                speed += acceleration*Time.deltaTime;
+            }
             rb.velocity = moveDirection * speed;
         }
+        /*
         else
         {
             rb.velocity = Vector2.zero; //stay still until next input.
         }
+        */
+       
     }
     private void SetMoveDirection(Vector2 dir) //sets the movement direction and disables input until a wall is hit.
     {
         moveDirection = dir;
         canMove = false; //start moving until hit a wall
 
-        //flip sprite (maybe this'll be finished if it is necessary. Currently just flips image from left to right).
-        if (dir == Vector2.left)
-            transform.localScale = new Vector3(-1, 1, 1);
+        //flip sprite (maybe this'll be finished if it is necessary. Currently just flips image from left to right). UNDRED CONSTRUCTION.
+        if (dir == Vector2.up)
+            sprite.transform.localScale = new Vector3(1, 1, 1);
+        else if (dir == Vector2.down)
+            transform.localScale = new Vector3(1, -1, 1);
+        else if (dir == Vector2.left)
+            sprite.transform.Rotate(0, 0, -90);
         else if (dir == Vector2.right)
-            transform.localScale = new Vector3(1, 1, 1);
+            sprite.transform.Rotate(0, 0, -90);
+            
+            
     }
     private void OnCollisionEnter2D(Collision2D collision) //called when colliding with a wall. Stops movement ONLY if the wall is blocking the current direction.
     {
+        speed=0;
+
         if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             Vector2 collisionNormal = collision.contacts[0].normal;
