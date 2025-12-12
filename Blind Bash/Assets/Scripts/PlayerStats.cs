e762095ddc5dc40d1f86673395bb2154f459abc1
fevpy <fevpy001@edu.xamk.fi>
2025-12-13 00:55:42 +0200

    Changed lose sound effect. Fixed an issue where it didn't play previously.

--- Blind Bash/Assets/Scripts/PlayerStats.cs
+++ Blind Bash/Assets/Scripts/PlayerStats.cs
@@ -85,6 +85,10 @@ public class PlayerStats : MonoBehaviour
             LevelUIManager.Instance.ShowLose(); //to show lose -canvas.
 
         }
+        else
+        {
+            AudioManager.Instance.PlaySFX("yelp");
+        }
     }
 
     public void ResetStats()
